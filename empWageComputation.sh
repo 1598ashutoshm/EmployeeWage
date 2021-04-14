f18b3e8105d346927c3bf6a3dee75a927ceb5754
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-14 22:42:20 +0530

    Updating: Calwageformonthcondition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,26 +6,28 @@ ispresentfulltime=1
 ispresentparttime=2
 EMP_RATE_PER_HR=20
 noofworkingdays=20
+maxworkinghrs=50
+
 totalworkinghrs=0
+totalworkingdays=0
 
-for (( day=1; day<=$noofworkingdays; day++ ))
+while [ $totalworkingdays  -lt $noofworkingdays ] &&
+      [ $totalworkinghrs  -lt $maxworkinghrs ]
 do
+     totalworkingdays=$(( $totalworkingdays + 1 ))
+     empcheck=$(( RANDOM % 3 ))   #0/1/2
+     #condition
+     case $empcheck in
+                 $ispresentfulltime) emphrs=8 ;;
+                 $ispresentparttime) emphrs=4 ;;
+                 *) emphrs=0 ;;
+     esac
+     totalworkinghrs=$(( $totalworkinghrs + $emphrs ))
 
-empcheck=$(( RANDOM % 3 ))   #0/1/2
-
-#condition
-
-case $empcheck in
-              $ispresentfulltime) emphrs=8 ;;
-              $ispresentparttime) emphrs=4 ;;
-              *) emphrs=0 ;;
-esac
-
-totalworkinghrs= $(( $totalworkinghrs + $emphrs ))
 #salary=$((  $EMP_RATE_PER_HR * $emphrs ))
+
 done
 
 salary=$((  $EMP_RATE_PER_HR * $totalworkinghrs ))
-
 
 
