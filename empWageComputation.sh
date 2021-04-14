5701bf7a8d2abc72ada1efbd0f5ef534cee88070
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-14 22:48:41 +0530

    Updating: functogetworkhours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,5 @@
-#! /bin/bash
-
-echo " Employee Wage Example "
+#! /bin/bash -x
+echo " Employee Wage Example using function "
 
 ispresentfulltime=1
 ispresentparttime=2
@@ -11,23 +10,29 @@ maxworkinghrs=50
 totalworkinghrs=0
 totalworkingdays=0
 
+#Function
+
+function getworkhours(){
+         local empcheck=$1
+         local emphrs=0
+         case $empcheck in
+                 $ispresentfulltime) emphrs=8 ;;
+                 $ispresentparttime) emphrs=4 ;;
+                 *) emphrs=0 ;;
+         esac
+         echo $emphrs
+
+}
+
 while [ $totalworkingdays  -lt $noofworkingdays ] &&
       [ $totalworkinghrs  -lt $maxworkinghrs ]
 do
      totalworkingdays=$(( $totalworkingdays + 1 ))
      empcheck=$(( RANDOM % 3 ))   #0/1/2
-     #condition
-     case $empcheck in
-                 $ispresentfulltime) emphrs=8 ;;
-                 $ispresentparttime) emphrs=4 ;;
-                 *) emphrs=0 ;;
-     esac
+     emphrs="$( getworkhours $empcheck )"
      totalworkinghrs=$(( $totalworkinghrs + $emphrs ))
 
 #salary=$((  $EMP_RATE_PER_HR * $emphrs ))
-
 done
-
 salary=$((  $EMP_RATE_PER_HR * $totalworkinghrs ))
-
 
