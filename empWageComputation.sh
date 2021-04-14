20dfb8678f6a540858dfbf13ee8b0127caca041b
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-14 22:24:15 +0530

    Modified: Parttime

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
 
-echo "Welcome to Employee Wage"
+#! /bin/bash
+echo " Employee Wage Example "
 
+ispresentfulltime=1
+ispresentparttime=2
 EMP_RATE_PER_HR=20
-empHr=8
 
-
-# constant
-IS_PRESENT=1
-
-#variable
-empcheck=$(( RANDOM%2 ))
+empcheck=$(( RANDOM % 3 ))   #0/1/2
 
 #condition
 
-if [ $empcheck -eq $IS_PRESENT ]
+if [ $empcheck -eq $ispresentfulltime ]
+then
+      emphrs=10
+elif [ $empcheck -eq $ispresentparttime ]
 then
       emphrs=8
-
 else
       emphrs=0
 fi
 
 salary=$(( $EMP_RATE_PER_HR * $emphrs ))
+
+echo $salary
+
+
